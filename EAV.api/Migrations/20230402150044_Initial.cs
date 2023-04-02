d838f6813c4ab19052dbd8f302d603a14870c0f0
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-04-02 12:01:12 -0300

    migration

---
+++ EAV.api/Migrations/20230402150044_Initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EAV.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:attribute_val_type", "text,integer,decimal,boolean,date,custom_object");
+
+            migrationBuilder.CreateTable(
+                name: "CustomEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomEntities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ValueType = table.Column<string>(type: "text", nullable: false),
+                    CustomEntityId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomAttributes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomAttributes_CustomEntities_CustomEntityId",
+                        column: x => x.CustomEntityId,
+                        principalTable: "CustomEntities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomValues",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ValueData = table.Column<string>(type: "text", nullable: false),
+                    CustomAttributeId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomValues_CustomAttributes_CustomAttributeId",
+                        column: x => x.CustomAttributeId,
+                        principalTable: "CustomAttributes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomAttributes_CustomEntityId",
+                table: "CustomAttributes",
+                column: "CustomEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomValues_CustomAttributeId",
+                table: "CustomValues",
+                column: "CustomAttributeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomValues");
+
+            migrationBuilder.DropTable(
+                name: "CustomAttributes");
+
+            migrationBuilder.DropTable(
+                name: "CustomEntities");
+        }
+    }
+}
